9004cb4f812a11bd6f3ac92a706ca8ca384774bc
stemjust4u <stemjust4u@gmail.com>
2023-09-09 17:29:13 -0400

    created dash charts

--- dash-charts.py
+++ dash-charts.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import dash
 from dash import dcc, html
 import plotly.express as px
+from dash.dependencies import Input, Output
 
 url = 'http://192.168.254.89:8086'
 token = 'root:root'
@@ -16,20 +17,29 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
     query_api = client.query_api()
     df = pd.DataFrame(client.query_api().query_data_frame('from(bucket: "esp2nred") |> range(start: -3d) |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'))
 
+#print(df)
+
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Sample Dash Dashboard"),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3, 4], 'y': [4, 3, 2, 1], 'type': 'bar', 'name': 'A'},
-                {'x': [1, 2, 3, 4], 'y': [2, 4, 1, 3], 'type': 'bar', 'name': 'B'},
-            ],
-            'layout': {
-                'title': 'Sample Bar Chart'
-            }
-        }
-    )
+    dcc.Graph(id='graph'),
+    dcc.Checklist(
+        id="checklist",
+        options=["1", "2", "3","4"],
+        value=["1", "2"],
+        inline=True
+    ),
 ])
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("checklist", "value"))
+def update_line_chart(sensor):
+    mask = df.location.isin(sensor)
+    fig = px.line(df[mask], 
+        x="_time", y="tempf", color='location')
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
